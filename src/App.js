6401e78e9042f8c7f332a0ee9a7eb24d5534062c
jisoleil <janggh1012@naver.com>
2023-12-07 16:14:00 +0900

    styled component5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import styled, { keyframes } from 'styled-components';
 
 const Wrapper=styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
 
 `;
 
@@ -21,6 +23,10 @@ const rotationAnimation = keyframes`
   }
 `;
 
+const Emoji=styled.span`
+  font-size: 36px;
+`;
+
 const Box=styled.div`
   height:200px;
   width:200px;
@@ -29,14 +35,9 @@ const Box=styled.div`
   justify-content: center;
   align-items: center;
   animation:${rotationAnimation} 1s linear infinite;
-  span{
-    font-size: 36px;
-    &:hover{
-      font-size: 40px;
-    }
-    &:active{
-      opacity: 0;
-    }
+  ${Emoji}:hover{
+   
+      font-size: 98px;
   }
 `
 
@@ -44,8 +45,9 @@ function App() {
   return (
     <Wrapper>
       <Box>
-        <span>😍</span>
+        <Emoji>😍</Emoji>
       </Box>
+      <Emoji>😂</Emoji>
     </Wrapper>
     );
 }
