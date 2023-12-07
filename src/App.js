0102bd3d2c581e284b027e9563c0d60195467714
jisoleil <janggh1012@naver.com>
2023-12-07 15:34:41 +0900

    styled component3

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ const Father=styled.div`
   display: flex;
 
 `;
-const Btn=styled.button`
-  color:white;
-  background-color:tomato;
-  border:0;
-  border-radius:15px;
+const Input = styled.input.attrs({required:true})`
+  background-color: tomato;
 `
 
 
 function App() {
   return (
   <Father as="header">
-    <Btn>Log in</Btn>
-    <Btn as="a" href='/'>Log in</Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
   </Father>
   );
 }
