9a32e547f330f7846a911adfc8181fd95762d369
jisoleil <janggh1012@naver.com>
2023-12-07 15:28:52 +0900

    style component as

--- src/App.js
+++ src/App.js
@@ -5,26 +5,19 @@ const Father=styled.div`
   display: flex;
 
 `;
-const Box=styled.div`
-  background-color:${(props)=>props.bgColor};
-  width:100px;
-  height:100px;
-`;
-const Circle=styled(Box)`
-  border-radius: 50px;
-`;
-
-const Text=styled.span`
+const Btn=styled.button`
   color:white;
-`;
+  background-color:tomato;
+  border:0;
+  border-radius:15px;
+`
+
 
 function App() {
   return (
-  <Father>
-    <Box bgColor="teal">
-      <Text>Hello</Text>
-    </Box>
-    <Circle bgColor="tomato"></Circle>
+  <Father as="header">
+    <Btn>Log in</Btn>
+    <Btn as="a" href='/'>Log in</Btn>
   </Father>
   );
 }
