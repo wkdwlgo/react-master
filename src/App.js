00bfd2169b4dc117f4fd73ed8b153db79a1ae91e
jisoleil <janggh1012@naver.com>
2023-12-07 15:59:16 +0900

    styled component4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const Father=styled.div`
+const Wrapper=styled.div`
   display: flex;
 
 `;
-const Input = styled.input.attrs({required:true})`
-  background-color: tomato;
-`
 
+const rotationAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  50%{
+    transform:rotate(360deg);
+    border-radius:100px;
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius:0px;
+  }
+`;
+
+const Box=styled.div`
+  height:200px;
+  width:200px;
+  background-color:tomato;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  animation:${rotationAnimation} 1s linear infinite;
+  span{
+    font-size: 36px;
+    &:hover{
+      font-size: 40px;
+    }
+    &:active{
+      opacity: 0;
+    }
+  }
+`
 
 function App() {
   return (
-  <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-  </Father>
-  );
+    <Wrapper>
+      <Box>
+        <span>😍</span>
+      </Box>
+    </Wrapper>
+    );
 }
 
 export default App;
