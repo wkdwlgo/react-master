e7abf2479c8b16237d92fb14fd5f2fd7588a2e6d
jisoleil <janggh1012@naver.com>
2023-12-07 16:33:26 +0900

    styled component6

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const darkTheme={
+  textColor:"whitesmoke",
+  backgroundColor:"#111"
+}
+
+const lightTheme={
+  textColor:"#111",
+  backgroundColor:"whitesmoke"
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
